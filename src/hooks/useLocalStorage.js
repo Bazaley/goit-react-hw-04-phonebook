0247d192b5c2259db02ef95f2cabc08cc91aa61d
Дмитрий <bazaley-dmitriy@i.ua>
2022-12-09 21:39:44 +0200

    fix useLocalStorage

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -2,15 +2,11 @@ import { useState, useEffect } from 'react';
 
 const useLocalStorage = key => {
   const [state, setState] = useState(() => {
-    const contacts = JSON.parse(localStorage.getItem(key));
-    if (!contacts) {
-      return [];
-    }
-    return contacts;
+    return JSON.parse(window.localStorage.getItem(key)) ?? [];
   });
 
   useEffect(() => {
-    localStorage.setItem(key, JSON.stringify(state));
+    window.localStorage.setItem(key, JSON.stringify(state));
   }, [state, key]);
 
   return [state, setState];
